0dd091c8dd07899e2dbf86c0eabf20954ad40ded
Maksym EE <ed819@protonmail.com>
2024-10-13 00:41:04 +0300

    Поменял сервер

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -1,8 +1,12 @@
 import axios, {AxiosInstance} from 'axios';
 
 
+//  Для WSL http://172.17.54.32:3000
+//  Для деплоя https://dd-v2.onrender.com
 export const axiosInstance: AxiosInstance = axios.create({
-                                                             baseURL: 'http://172.17.54.32:3000',
+
+
+                                                             baseURL: 'https://dd-v2.onrender.com',
                                                              timeout: 0,
                                                              headers: {
                                                                  'Content-Type': 'application/json',
