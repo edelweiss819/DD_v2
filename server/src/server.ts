a48105dc7ec275afeb42f0ad233614a7fd8ebd38
Maksym EE <ed819@protonmail.com>
2024-10-13 00:17:19 +0300

    Добавил подключение к бэку

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
+import articlesRouter from './routes/articlesRouter';
+import metadataRouter from './routes/metadataRouter';
+import {
+    updateGenresCount,
+    updateTotalArticlesCount
+} from './controllers/metadataController';
+import {UPDATE_GENRES_COUNT, UPDATE_TOTAL_ARTICLES} from './constants';
+import usersRouter from './routes/usersRouter';
+import authRouter from './routes/authRouter';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,15 +18,25 @@ const app = express();
 const PORT = Number(process.env.PORT) || 10000;
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Сервер запущен!');
-});
+app.use(articlesRouter);
+app.use(metadataRouter);
+app.use(usersRouter);
+app.use(authRouter);
+
+
+const uri = process.env.MONGODB_URI;
+
 
 const startServer = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI!);
-        console.log('Connected to the database');
+        await mongoose.connect(uri!);
+        await mongoose.connection.db.admin().command({ping: 1});
+        console.log('Pinged your deployment. You successfully connected to MongoDB!');
+
+        await updateTotalArticlesCount(UPDATE_TOTAL_ARTICLES);
+        await updateGenresCount(UPDATE_GENRES_COUNT);
 
         app.listen(PORT, '0.0.0.0', () => {
             console.log(`Server is running on port: ${PORT}`);
@@ -26,4 +46,6 @@ const startServer = async () => {
     }
 };
 
-startServer();
+startServer().catch(err => {
+    console.error('Failed to start server:', err);
+});
