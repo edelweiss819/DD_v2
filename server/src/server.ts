ef746ad8ff838d9f7e109deedf9feadf5649045b
Maksym EE <ed819@protonmail.com>
2024-10-12 23:03:39 +0300

    Попытка деплоя бэка 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,36 +1,24 @@
 import express from 'express';
-import cors from 'cors';
 import mongoose from 'mongoose';
-import articlesRouter from './routes/articlesRouter';
-import metadataRouter from './routes/metadataRouter';
-import {
-    updateGenresCount,
-    updateTotalArticlesCount
-} from './controllers/metadataController';
-import {UPDATE_GENRES_COUNT, UPDATE_TOTAL_ARTICLES} from './constants';
-import usersRouter from './routes/usersRouter';
-import authRouter from './routes/authRouter';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
-app.use(cors());
 
-app.use(articlesRouter);
-app.use(metadataRouter);
-app.use(usersRouter);
-app.use(authRouter);
+app.get('/', (req, res) => {
+    res.send('Сервер запущен!');
+});
 
 const startServer = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/articles');
-        console.log(`Connected to the database`);
+        await mongoose.connect(process.env.MONGODB_URI!);
+        console.log('Connected to the database');
 
-        await updateTotalArticlesCount(UPDATE_TOTAL_ARTICLES);
-        await updateGenresCount(UPDATE_GENRES_COUNT);
-
-        app.listen(PORT, () => {
+        app.listen(PORT, '0.0.0.0', () => {
             console.log(`Server is running on port: ${PORT}`);
         });
     } catch (err) {
@@ -38,7 +26,4 @@ const startServer = async () => {
     }
 };
 
-
-startServer().catch(err => {
-    console.error('Failed to start server:', err);
-});
+startServer();
