08ecd7615583c9ee475f6933c778e111229d70f3
Maksym EE <ed819@protonmail.com>
2024-10-12 23:03:39 +0300

    Попытка деплоя бэка 2

--- server/src/constants/flags.ts
+++ server/src/constants/flags.ts
@@ -1,4 +1,4 @@
 // On/off updating total articles count in db
-export const UPDATE_TOTAL_ARTICLES: boolean = false;
+export const UPDATE_TOTAL_ARTICLES: boolean = true;
 //On/off updating total articles by every genre
-export const UPDATE_GENRES_COUNT: boolean = false;
+export const UPDATE_GENRES_COUNT: boolean = true;
