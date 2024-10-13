8d31066d174992799628d920f4f152ed488e925e
Maksym EE <ed819@protonmail.com>
2024-10-13 20:55:21 +0300

    Починил избранное

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -6,7 +6,7 @@ import axios, {AxiosInstance} from 'axios';
 export const axiosInstance: AxiosInstance = axios.create({
 
 
-                                                             baseURL: 'https://dd-v2.onrender.com,
+                                                             baseURL: 'https://dd-v2.onrender.com',
                                                              timeout: 0,
                                                              headers: {
                                                                  'Content-Type': 'application/json',
