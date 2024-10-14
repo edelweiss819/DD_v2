c810f37aef58da47985a5b3ba3d204d35743abf5
Maksym EE <ed819@protonmail.com>
2024-10-14 09:47:46 +0300

    Изменил конфиг

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -1,8 +1,6 @@
 import axios, {AxiosInstance} from 'axios';
 
 
-//  Для WSL http://172.17.54.32:3000
-//  Для деплоя https://dd-v2.onrender.com
 export const axiosInstance: AxiosInstance = axios.create({
 
 
