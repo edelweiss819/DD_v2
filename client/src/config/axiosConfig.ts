72414e8521fbc3d05c92ac4e9bc5a1541630d848
Maksym EE <ed819@protonmail.com>
2024-10-14 10:08:35 +0300

    Изменил конфиг

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -1,10 +1,11 @@
 import axios, {AxiosInstance} from 'axios';
+import * as process from 'node:process';
 
 
 export const axiosInstance: AxiosInstance = axios.create({
 
 
-                                                             baseURL: 'https://dd-v2.onrender.com',
+                                                             baseURL: process.env.BACK_URI,
                                                              timeout: 0,
                                                              headers: {
                                                                  'Content-Type': 'application/json',
