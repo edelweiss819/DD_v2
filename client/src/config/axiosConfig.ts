bdfbe8361ee404bc5fe4ac378170c2ead94cf7fd
Maksym EE <ed819@protonmail.com>
2024-10-14 10:18:07 +0300

    Изменил конфиг

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -1,11 +1,7 @@
 import axios, {AxiosInstance} from 'axios';
-import * as process from 'node:process';
-
 
 export const axiosInstance: AxiosInstance = axios.create({
-
-
-                                                             baseURL: process.env.BACK_URI,
+                                                             baseURL: import.meta.env.VITE_BACK_URI,
                                                              timeout: 0,
                                                              headers: {
                                                                  'Content-Type': 'application/json',
